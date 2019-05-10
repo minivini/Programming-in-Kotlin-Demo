dbf0f72be1cf1e43f0f9fa0f51612aa9890a27d4
Ofir Ron <>
2019-05-10 14:34:10 +0300

    Pair and Triple (1.5)

--- src/main.kt
+++ src/main.kt
@@ -35,7 +35,8 @@ fun main(args: Array<String>) {
     println(length)
 
 
-    // Kotlin Basics 1.4
+    // Kotlin Basics (1.4)
+    println("*****************")
 
     // This a comment. It is not executed
 
@@ -56,4 +57,31 @@ fun main(args: Array<String>) {
 
      * Back to the first,
      */
+
+    // Pair and Triple (1.5)
+    println("*****************")
+    val coordinates = Pair(2,3)
+    val (x,y) = coordinates
+    println("x = $x y = $y")
+
+    println("x = ${coordinates.first} y = ${coordinates.second}")
+
+    val coordinatesDoubles = Pair(2.1, 3.5)
+    println("x = ${coordinatesDoubles.first} y = ${coordinatesDoubles.second}")
+
+    val coordinatesMixed = Pair(2.1, 3)
+    println("x = ${coordinatesMixed.first} y = ${coordinatesMixed.second}")
+
+    val x1 = coordinates.first
+    val y1 = coordinates.second
+    println("x = ${x1} y = ${y1}")
+
+    val coordinates3D = Triple(2,3,1)
+    val (x3, y3, z3) = coordinates3D
+    println("x = ${x3} y = ${y3} z = $z3")
+
+    val (x4, y4, _) = coordinates3D
+    println("x4 = $x4 y4 = $y4")
+
+
 }
