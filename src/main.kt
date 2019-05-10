cdc92eb0e0bbbe1433349e99fcd4a4a27f004ff0
Ofir Ron <>
2019-05-10 15:33:41 +0300

    - copy from challenge file

--- src/main.kt
+++ src/main.kt
@@ -82,6 +82,23 @@ fun main(args: Array<String>) {
 
     val (x4, y4, _) = coordinates3D
     println("x4 = $x4 y4 = $y4")
+}
 
+/**
+ *
+ */
+fun Challenge_Pair_and_Triple_1_6(args: Array<String>) {
+/* Pairs and Triples
+
+ Declare a constant Pair that contains two Int values. Use this to represent a date (month, day).
+ */
+
+/*
+ In one line, read the day and month values into two constants.
+ */
+
+
+/*
+ Now create a Triple using the month, day and year */
 
 }
